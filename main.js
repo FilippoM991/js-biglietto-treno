1bc426051841719927fb241acd9333a95a7fe77a
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-11-14 18:09:38 +0100

    aggiunto fixed per arrotondare e parseInt

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-var kilometri = prompt('Quanti km devi percorrere?');
-var eta = prompt('Quanti anni hai?');
-console.log(kilometri);
-console.log(eta);
+var kilometri = parseInt (prompt('Quanti km devi percorrere?(Scrivi un numero intero e mi raccomando niente lettere, altrimenti ti facciamo pagare di più!)'));
+var eta = parseInt (prompt('Quanti anni hai? (Scrivi un numero intero e mi raccomando niente lettere, altrimenti ti facciamo pagare di più!)'));
+// console.log(kilometri);
+// console.log(eta);
 document.getElementById('km-viaggio').innerHTML = kilometri;
 document.getElementById('eta-utente').innerHTML = eta;
 var prezzoBiglietto = (kilometri * 0.21);
 if (eta < 18) {
-    prezzoBiglietto = (kilometri * 0.21) - (kilometri * 0.21) * 20 / 100;
+    prezzoBiglietto = (kilometri * 0.21) - ((kilometri * 0.21) * 20) / 100;
 } else if (eta > 65) {
     prezzoBiglietto = (kilometri * 0.21) - (kilometri * 0.21) * 40 / 100;
 }
-console.log(prezzoBiglietto);
-document.getElementById('prezzo-finale').innerHTML = prezzoBiglietto;
+// if (isNaN(prezzoBiglietto)) {
+//     document.getElementById('prezzo-finale').innerHTML = "Riprova a inserire i dati, ricorda..solo numeri interi, niente lettere!";
+// }
+// il comando sopra con if non funziona!
+// console.log(prezzoBiglietto);
+var prezzoBigliettoArrotondato = prezzoBiglietto.toFixed(2);
+
+document.getElementById('prezzo-finale').innerHTML = prezzoBigliettoArrotondato;
